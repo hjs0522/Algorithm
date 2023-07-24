04a8f060543d859dc72e2b9404f33dc21b5e5428
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2023-07-24 23:49:21 +0900

    7월24일

--- 디펜스게임.py
+++ 디펜스게임.py
@@ -6,12 +6,14 @@ def solution(n, k, enemy):
         if idx == len(enemy):
             answer = idx
             return
-        if enemy[idx] >n and k == 0:
-            answer = max(answer,idx-1)
-            return
-        dfs(n - enemy[idx],k,idx+1,enemy)
+        if n-enemy[idx]>=0:
+            dfs(n - enemy[idx],k,idx+1,enemy)
         if k>0:
             dfs(n,k-1,idx+1,enemy)
+        
+        if enemy[idx] > n and k == 0:
+            answer = max(answer,idx)
+            return
         
     dfs(n,k,0,enemy)
     return answer
